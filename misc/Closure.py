acf6664e4e789077c39e2e33502dc6d68c5493cd
johnxguo(郭晛) <johnxguo@tencent.com>
2018-11-13 11:36:10 +0800

    Closure.py

---
+++ misc/Closure.py
@@ -0,0 +1,13 @@
+
+
+def fun(num):
+    print(num)
+
+def create_fun(num):
+    def fun_bound():
+        return fun(num)
+    return fun_bound
+
+funs = [create_fun(num) for num in range(1,10)]
+for fu in funs:
+    fu()
